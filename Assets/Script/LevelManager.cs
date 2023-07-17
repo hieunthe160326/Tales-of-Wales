54927c4e5a7480b32dbd0d967dec379177ff7091
thailshe160614 <thailshe160614@fpt.edu.vn>
2023-07-18 06:18:26 +0700

    fix scene change and main menu

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    [SerializeField] private GameObject _loaderCanvas;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public async void LoadScene(string sceneName)
+    {
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
+
+        _loaderCanvas.SetActive(true);
+
+        await Task.Delay(1000);
+
+        scene.allowSceneActivation = true;
+        _loaderCanvas.SetActive(false);
+    }
+}
